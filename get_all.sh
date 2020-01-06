05446eb168fc8e6886bb9eb4e4c18c6f879435ca
gingerbeardman <matt.sephton@gmail.com>
2020-01-06 13:46:26 +0000

    remove commented out code

--- get_all.sh
+++ get_all.sh
@@ -6,9 +6,3 @@ while read url
 do
 	scrapy crawl -a start_urls=$url iwata-eu
 done < "$@"
-
-#cd _html
-#pandoc -o "Iwata Asks.epub" --toc --toc-depth=1 --metadata-file=meta/metadata.yaml --css=css/iwata.css \
-#       -i "html/*.html"
-#open "Iwata Asks.epub"
-#cd ..
