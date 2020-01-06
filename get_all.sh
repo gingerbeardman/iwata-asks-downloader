697659ea3642f11144935c6d87076e1d31d880eb
gingerbeardman <matt.sephton@gmail.com>
2020-01-06 14:46:48 +0000

    Add check for Python 3

--- get_all.sh
+++ get_all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[[ "$(python -V)" =~ "Python 3" ]] || echo "Iwata Asks Downloader: please make sure Python 3 is installed as default" || exit;
+
 rm ./_md/*.md
 rm ./_html/*.html
 while read url
