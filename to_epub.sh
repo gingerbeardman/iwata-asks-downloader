2c0b7bd5826a2f3a281ecb1d759d82ca9f289cc4
gingerbeardman <matt.sephton@gmail.com>
2020-01-06 18:34:51 +0000

    Clean epub directory before generation

--- to_epub.sh
+++ to_epub.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm ./_epub/*.epub
 cd _html
 find ./ -maxdepth 1 -iname "*.html" -type f -exec sh -c 'pandoc "${0}" --toc --toc-depth=1 --metadata-file=meta/metadata.yaml --css=css/epub.css -o "../_epub/${0%.html}.epub"' {} \;
 cd ..
